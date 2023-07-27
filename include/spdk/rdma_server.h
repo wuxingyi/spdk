af09f2510438019e42eabd18e6ace0cb7ae2c2a7
wuxingyi <wuxingyi2015@outlook.com>
2023-07-27 15:55:45 +0800

    remove unused
    Signed-off-by: wuxingyi <wuxingyi2015@outlook.com>

--- include/spdk/rdma_server.h
+++ include/spdk/rdma_server.h
@@ -224,7 +224,6 @@ extern "C"
 	struct spdk_srv_transport_opts
 	{
 		uint16_t max_queue_depth;
-		uint16_t max_conns_per_tgt;
 		uint32_t in_capsule_data_size;
 		/* used to calculate mdts */
 		uint32_t max_io_size;
